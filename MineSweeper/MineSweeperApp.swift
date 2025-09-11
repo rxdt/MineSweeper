5d5cd5f9e6bc7d7e6d03daa90229f6a85186b8c9
Roxana del Toro <rxdeltoro@gmail.com>
2025-09-10 19:13:33 -0700

    Initial Commit

---
+++ MineSweeper/MineSweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineSweeperApp.swift
+//  MineSweeper
+//
+//  Created by Roxana del Toro Lopez on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MineSweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MineSweeperTests/MineSweeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MineSweeperTests.swift
+//  MineSweeperTests
+//
+//  Created by Roxana del Toro Lopez on 9/10/25.
+//
+
+import Testing
+@testable import MineSweeper
+
+struct MineSweeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
